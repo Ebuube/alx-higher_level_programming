f550cbd730483dfff9f4f802f5544b93da21f3a9
vagrant <vagrant@vagrant.vm>
2022-11-29 00:47:23 +0000

    Updated the file 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
     language that combine remarkable power with very clear syntax"
-str = str[39:66]  + " " + str[109:113] + " " + str[:6]
+str = str[39:67] + str[109:114] + str[:6]
 print(str)
