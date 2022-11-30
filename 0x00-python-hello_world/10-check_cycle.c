cdcb2726d2a26ffb131153cdf1fbd9ba8ea58d38
vagrant <vagrant@vagrant.vm>
2022-11-30 18:48:43 +0000

    Made the function in file 10-check_cycle.c efficient using Floyd's Cycle Detection Algorithm

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,38 +1,39 @@
 #include "lists.h"
-#include "clist.h"
-#include "cfuncs.c"
 
 /**
- * check_cycle - checks if there is a cycle in a listint_t list
+ * check_cycle - checks if a listint_t list has a loop
  * @list: points to a listint_t list
+ *
+ * Description: The function uses two pointers. One moves one step at a time
+ * while the second pointer moves two nodes at a time.
+ * The logic is that if there is a loop in the list, then the two pointers
+ * will definitely meet at a point in the loop. Else, one of them will
+ * hit the null pointer
  *
  * Return: 0 if there is no cycle, 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
 {
-	clist_t *clist = 0;
-	int cycle = 0;
+	listint_t *slow = 0;
+	listint_t *fast = 0;
+	int found_loop = 0;
 
 	if (!list)
 	{
-		printf("ERROR: NULL passed as list\n");
-		return (cycle);
+		/* empty list */
+		return (0);
 	}
-
-	for (clist = 0; list; list = list->next)
+	
+	for (slow = list->next, fast = list->next->next, found_loop = 0;
+		slow && fast && fast->next;
+		slow = slow->next, fast = fast->next->next)
 	{
-		if (isin(list, clist))
+		if (slow == fast)
 		{
-			cycle = 1;
+			found_loop = 1;
 			break;
 		}
-		if (!add_node_clist(list, &clist))
-		{
-			printf("ERROR: could not add node\n");
-			return(cycle);
-		}
 	}
-	free_clist(&clist);
-
-	return (cycle);
+	
+	return (found_loop);
 }
