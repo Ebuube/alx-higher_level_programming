b32a337c06be0e7b27b6c2e2b86e1f68bd8d4437
vagrant <vagrant@vagrant.vm>
2022-12-01 14:40:35 +0000

    Updated the file 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,7 @@
 """To print comma delimited integers from 0 to 99"""
 
 for num in range(0, 100):
-    if num < 99:
-        print("{:02}".format(num), end=", ")
-    else:
+    if num == 99:
         print("{}".format(num))
+    else:
+        print("{:02}".format(num), end=", ")
