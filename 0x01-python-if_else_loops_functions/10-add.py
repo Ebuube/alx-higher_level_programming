83a166c29e76c678020090fdd2ec6d26a9087d7b
vagrant <vagrant@vagrant.vm>
2022-12-02 22:33:56 +0000

    Updated a file

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -5,11 +5,5 @@ A function that adds two integers and returns the result
 
 
 def add(a, b):
-    try:
-        a = int(a)
-        b = int(b)
-    except ValueError:
-        exit(1)
-
     return (a + b)
 # end of function
