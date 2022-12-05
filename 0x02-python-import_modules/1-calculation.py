de2c7571100048d17333e38fd6312642eda13b6f
vagrant <vagrant@vagrant.vm>
2022-12-05 16:34:02 +0000

    Updated a file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    print("{} + {} = {}".format(str(a), str(b), str(add(a, b))))
+    print("{} - {} = {}".format(str(a), str(b), str(sub(a, b))))
+    print("{} * {} = {}".format(str(a), str(b), str(mul(a, b))))
+    print("{} / {} = {}".format(str(a), str(b), str(div(a, b))))
