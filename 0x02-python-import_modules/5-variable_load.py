0252c5f3819ae12aa586972737106b7bcfddf638
vagrant <vagrant@vagrant.vm>
2022-12-05 21:16:44 +0000

    Created a new file

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+A program that imports the variable `a` from the file `variable_load_5.py`
+and prints its value
+"""
+
+
+if __name__ == '__main__':
+    from variable_load_5 import a
+    print(a)
