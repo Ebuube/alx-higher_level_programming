ff3723e98adf4d8ae6fe87b1acd751c4e2ebbc14
Ebube Gideon Onwuta <111176684+Ebuube@users.noreply.github.com>
2022-12-05 22:59:34 +0100

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -14,8 +14,11 @@ if __name__ == '__main__':
     a = 10
     b = 5
 
-
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    my_str = "{} + {} =".format(a, b)
+    print(my_str, add(a, b))
+    my_str = "{} - {} =".format(a, b)
+    print(my_str, sub(a, b)
+    my_str = "{} * {} =".format(a, b)
+    print(my_str, mul(a, b))
+    my_str = "{} / {} =".format(a, b)
+    print(my_str, div(a, b))
