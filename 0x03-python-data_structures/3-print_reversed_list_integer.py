473aac5e261dd78d8cef298873a7f9aa339baa4f
vagrant <vagrant@vagrant.vm>
2022-12-06 23:10:38 +0000

    Corrected pycodestyle error

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,7 +5,7 @@ A function that prints all integers of a list in reversed order.
 
 
 def print_reversed_list_integer(my_list=[]):
-    if (my_list == None) or (len(my_list) == 0):
+    if (my_list is None) or (len(my_list) == 0):
         return (None)
     my_list.reverse()
     for num in my_list:
