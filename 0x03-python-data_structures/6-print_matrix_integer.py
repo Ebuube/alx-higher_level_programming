c7687d8005dd1ed14de44ef8819ef5079939ff7b
vagrant <vagrant@vagrant.vm>
2022-12-07 08:47:33 +0000

    Updated some files

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -10,9 +10,9 @@ def print_matrix_integer(matrix=[[]]):
         count2 = 0
         if (len(matrix[count1]) != 0):
             while count2 < (len(matrix[count1]) - 1):
-                print("{} ".format(matrix[count1][count2]), end=' ')
+                print("{:d} ".format(matrix[count1][count2]), end=' ')
                 count2 += 1
-            print("{}".format(matrix[count1][count2]))
+            print("{:d}".format(matrix[count1][count2]))
         count1 += 1
     if (count1 == 1) and (count2 == 0):    # empty matrix
         print()
