95bc9250289bee82b418b17740e780522a6fa49b
vagrant <vagrant@vagrant.vm>
2022-12-20 03:39:28 +0000

    Created new file and README.md

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    """A function that prints x elements of a list
+    Args:
+        my_list: can contain any type (integer, string, etc.)
+        x: number of the elements to print
+    
+    Returns:
+        The real number of elements printed
+    """
+    count = 0
+    for count in range(x):
+        try:
+            print(my_list[count], end='')
+            count += 1
+        except IndexError:
+            break
+        except TypeError:
+            break
+
+    print()
+    return (count)
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,7 @@
+```
+ONWUTA EBUBE GIDEON
+```
+
+# Description of the Files in this Directory
+
+[0-safe_print_list.py](./0-safe_print_list): a function that safely prints `x` elements of a list
