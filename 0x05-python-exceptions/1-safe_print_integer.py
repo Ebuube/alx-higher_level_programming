533e4e77ffbd0ee228f4e6ac1bdcd852517687dc
vagrant <vagrant@vagrant.vm>
2022-12-20 03:48:01 +0000

    Solved task 2 and updated README.md

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """A function that safely prints an integer with `"{:d}".format().`
+    Args:
+        value: an type of value
+
+    Returns:
+        True if integer is printed else Fals
+    """
+    try:
+        print("{:d}".format(value))
+    except ValueError:
+        return (False)
+
+    return (True)
--- 0x05-python-exceptions/README.md
+++ 0x05-python-exceptions/README.md
@@ -5,3 +5,7 @@ ONWUTA EBUBE GIDEON
 # Description of the Files in this Directory
 
 [0-safe_print_list.py](./0-safe_print_list): a function that safely prints `x` elements of a list
+
+
+
+[1-safe_print_integer.py](./1-safe_print_integer.py): a function that prints an integer with `"{:d}".format()`.
