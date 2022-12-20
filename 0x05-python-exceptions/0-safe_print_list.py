5e9a2e623fb22a9e543a8bc30a78ad24b0193077
vagrant <vagrant@vagrant.vm>
2022-12-20 03:41:49 +0000

    Corrected pycodestyle error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     Args:
         my_list: can contain any type (integer, string, etc.)
         x: number of the elements to print
-    
+
     Returns:
         The real number of elements printed
     """
