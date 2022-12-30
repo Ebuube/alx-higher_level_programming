0e7b03b670e27d6389ab3675c7912259df0bbf48
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2022-12-30 09:34:24 +0000

    Corrected Inside result in this file

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -13,7 +13,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except (ZeroDivisionError, TypeError, ValueError):
-        return (None)
+        result = None
     finally:
         print("Inside result: {}".format(result))
 
