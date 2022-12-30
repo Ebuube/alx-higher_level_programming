dc046b0719fc7bf2c8ba4753d2476d5657a2984c
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2022-12-30 02:08:53 +0000

    Corrected exceptions in the file 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -18,7 +18,10 @@ def safe_print_integer_err(value):
 
     try:
         print("{:d}".format(value))
-    except ValueError as te:
+    except ValueError as ve:
+        print("Exception: " + str(ve), file=sys.stderr)
+        return (False)
+    except TypeError as te:
         print("Exception: " + str(te), file=sys.stderr)
         return (False)
 
