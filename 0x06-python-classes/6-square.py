71f713f422b65f76d3fb139c2813cc3ac8aa7df8
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-01-03 04:20:32 +0000

    Corrected output of 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -135,7 +135,7 @@ class Square:
                 print()
             for y_axis in range(self.__size):
                 for x_pos in range(self.__position[0]):
-                    print('_', end='')
+                    print(' ', end='')
                 for x_axis in range(self.__size):
                     print('#', end='')
                 print()
