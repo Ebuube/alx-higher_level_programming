e4267f10f25c9c3654b9d80c24141f6922c9ea56
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-01-05 01:25:13 +0000

    Modified the docstrings in file 0-add_integer.py and the test cases in ./tests/0-add_integer.txt to statisfy requirements by ALX

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,29 +6,8 @@ Attriutes:
 """
 
 def add_integer(a, b=98):
-    """Add two integers
-
-    Args:
-        a (int or float): first number
-        b (int or float): second number an optional argument with
-                            default value of 98
-
-    Returns:
-        An integer which is the sum of a and b
-
-    Usage:
-        >>> add_integer(2, 8)
-        10
-
-        On missing the second argument which is optional,
-        the default value of 98 is used in its place
-
-        >>> add_integer(5)
-        103
-
-    Note:
-        A  TypeError is raised if the arguments are of any other types
-        than integer or float
+    """Return the sum of two integers
+        A TypeError is raised if the arguments are not integer or float
     """
 
     if isinstance(a, int) or isinstance(a, float):
