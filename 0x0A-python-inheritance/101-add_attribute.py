48516b99487ff8b601cb8bc50cd81b8aa64a8f21
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-02-26 06:58:46 +0000

    Corrected pycodestyle error in 101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -2,6 +2,7 @@
 """This module contains a method
 """
 
+
 def add_attribute(obj, attr, val):
     """Add the attribute ``attr`` with value ``val`` to the object ``obj``
     Raises TypeError if impossible to add new attribute
