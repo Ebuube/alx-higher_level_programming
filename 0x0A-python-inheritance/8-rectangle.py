14abc64cdcd88c961c3520f39e6869271a025913
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-02-26 05:46:53 +0000

    Corrected pycodestyle errors in 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,6 +5,8 @@ and another class ``Rectangle`` which inherits from ``BaseGeometry``
 
 
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
 class Rectangle(BaseGeometry):
     """Define the class
     """
