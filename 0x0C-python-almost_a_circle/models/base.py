7f1d6819f988599ccbc7673d10e7b98071ce0b9b
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-03-08 15:22:36 +0000

    made a method static in Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,6 +27,7 @@ class Base():
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
         Return the JSON string representation of the argument
