cf2e2ed5caf6d6873b6b16e5b756c19b168271b4
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-04-27 07:58:11 +0000

    corrections

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,7 @@
 -- Convert a databasee to UTF8MB4, COLLATE UTF8MB4_UNICODE_CI
-ALTER DATABASE CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+USE `hbtn_0c_0`;
+
 ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-SELEECT CONVERT (`name` USING utf8mb4) AS `name` FROM `first_table`;
+-- SELECT CONVERT (`name` USING utf8mb4) AS `name` FROM `first_table`;
