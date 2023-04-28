51d793c7a2c267b47067d023ab9ccae8244107b7
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-04-28 13:49:11 +0000

    Updated a file

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,5 +2,5 @@
 -- Grant only 'SELECT' privilege in the database 'hbtn_0d_2' to the user
 
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
-CREATE USER 'user_0d_2' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2';
