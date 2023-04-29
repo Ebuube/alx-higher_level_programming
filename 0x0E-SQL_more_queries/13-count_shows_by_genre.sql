47c401ce6dfc35129620207b082dce7e1781e567
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-04-29 15:51:26 +0000

    Solved task 13 - to be verified

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- List all genres from the database and display the number of shows linked to each
+SELECT tv_genres.name as 'TV Show genre',
+	count(tv_genres.id) as 'Number of shows linked to this genre'
+FROM tv_genres
+JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.id
+ORDER BY 'Number of shows linked to this genre' DESC;
