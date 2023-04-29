0a41870562bf671ae0e1d79e297591b5ae7ae7b3
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-04-29 11:07:05 +0000

    Corrected missing element of table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,8 +4,9 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
 	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	CONSTRAINT cities_pk PRIMARY KEY (id),
-	CONSTRAINT cities_states_fk FOREIGN KEY (id)
+	CONSTRAINT cities_states_fk FOREIGN KEY (state_id)
 	REFERENCE states (id)
 );
