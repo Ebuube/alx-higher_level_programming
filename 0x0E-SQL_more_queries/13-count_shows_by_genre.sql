d72d2c44131c9b33468e6b5b9f579324dad26008
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-04-29 15:57:23 +0000

    Corrected wrong name for columns

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- List all genres from the database and display the number of shows linked to each
-SELECT tv_genres.name AS 'TV Show genre',
-	count(tv_genres.id) AS 'Number of shows linked to this genre'
+SELECT tv_genres.name AS 'genre',
+	count(tv_genres.id) AS 'number_of_shows'
 FROM tv_genres
 JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.id
-ORDER BY 'Number of shows linked to this genre' DESC;
+ORDER BY 'number_of_shows' DESC;
