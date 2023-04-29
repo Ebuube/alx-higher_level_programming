85e21b191b31ac4c6aafe64c26054344e5a9df01
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-04-29 15:54:21 +0000

    Capitalized all SQL keywords

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- List all genres from the database and display the number of shows linked to each
-SELECT tv_genres.name as 'TV Show genre',
-	count(tv_genres.id) as 'Number of shows linked to this genre'
+SELECT tv_genres.name AS 'TV Show genre',
+	count(tv_genres.id) AS 'Number of shows linked to this genre'
 FROM tv_genres
 JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
