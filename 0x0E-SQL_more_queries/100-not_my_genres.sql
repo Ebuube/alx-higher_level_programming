5c2239b057fdf92bf8a0b2afc67a47af7009b954
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-05-01 21:32:12 +0000

    solved task 17 new

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,13 +1,14 @@
 -- List all genres not linked to the show 'Dexter'
 
 WITH tb_temp AS (
-	SELECT tv_genres.name
-	FROM (tv_genres
-		JOIN tv_show_genres
-		ON tv_genres.id = tv_show_genres.genre_id)
-	JOIN tv_shows
-	ON tv_shows.id = tv_show_genres.show_id
-	AND tv_shows.title NOT IN ('Dexter'))
-SELECT DISTINCT tb_temp.name
+	SELECT *
+	FROM tv_genres
+	JOIN tv_show_genres
+	ON tv_show_genres.genre_id = tv_genres.id)
+SELECT tb_temp.name
 FROM tb_temp
-ORDER BY tb_temp.name ASC;
+JOIN tv_shows
+ON tv_shows.title != 'Dexter'
+AND tv_shows.id = tb_temp.show_id
+GROUP BY tb_temp.name
+ORDER BY tb_temp.name;
