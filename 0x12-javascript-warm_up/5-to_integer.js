dc764e89ac4bab4286ae599a4acda670bb125504
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-05-22 22:13:03 +0000

    Completed solving task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,5 +2,13 @@
 
 // An integer
 
-// const cmdLineArgs = process.argv.slice(2);
+const cmdLineArgs = process.argv.slice(2);
 // remove the first two arguments
+
+const number = parseInt(cmdLineArgs[0]);
+
+if (isNaN(number)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: {}', number);
+}
