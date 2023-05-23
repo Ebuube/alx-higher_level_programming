a1aa7e008ae4cdb02b382349df4d3d8c27cb1e48
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-05-23 15:43:45 +0000

    corrected output

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -10,5 +10,5 @@ const number = parseInt(cmdLineArgs[0]);
 if (isNaN(number)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ', number);
+  console.log('My number: ' + number);
 }
