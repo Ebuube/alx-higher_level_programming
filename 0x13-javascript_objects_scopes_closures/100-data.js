8a8cb0d33e1731812c8f1d39df824670efeed44f
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-05-28 11:11:25 +0000

    Corrected task 11 to fetch index of element being accessed

--- 0x13-javascript_objects_scopes_closures/100-data.js
+++ 0x13-javascript_objects_scopes_closures/100-data.js
@@ -1,2 +1,2 @@
 #!/usr/bin/node
-exports.list = [1, 2, 3, 4, 5];
+exports.list = [12, 3, 5, 12, -4, 9];
--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -7,7 +7,7 @@ const list = require('./100-data.js').list;
 compute(list);
 
 function compute (list) {
-  const newList = list.map((num) => num * list.indexOf(num));
+  const newList = list.map((num, index) => num * index);
   console.log(list);
   console.log(newList);
 }
