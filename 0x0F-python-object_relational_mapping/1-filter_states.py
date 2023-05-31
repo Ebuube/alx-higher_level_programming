de75afd1f1a1c48fcf3b9225c4b2b9a7b07ae71f
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-05-31 02:18:48 +0000

    Satisfied conditions for task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -70,7 +70,7 @@ db = MySQLdb.connect(
 cur = db.cursor()
 
 Run(cur.execute, """SELECT id, name FROM {}
-        WHERE name LIKE 'N%'
+        WHERE name LIKE 'N%' or name LIKE 'n%'
         ORDER BY states.id;
         COMMIT""".format(MY_TABLES['states']))
 
