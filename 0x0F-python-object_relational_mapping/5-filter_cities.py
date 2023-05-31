f74c4fdb2b30d2fd8cba092093f5a8be5b839eea
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-05-31 22:36:34 +0000

    Solved task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python3
+"""
+This script takes in the name of a state as an argument
+and lists all `cities` of that state, using the database `hbtn_0e_4_usa`
+
+Results are sorted in ascending order by cities.id
+
+The queries are SQL Injection Free!
+"""
+
+import MySQLdb
+import sys
+
+
+"""
+IMPORTANT FUNCTIONS AND DECLARATIONS
+"""
+
+
+def Run(command=None, statement='', params=()):
+    """
+    Run - execute a MySQLdb command
+    command: the command to run on a cursor object example
+        `cur.execute`
+
+    If there is no statement, pass command as it is, example
+        `cur.fetchall`
+        where it actually means `cur.fetchall()`
+
+    statement: The statement to run
+    params (tuple): the parameters to substitute in the statement
+    """
+    return_value = None
+    try:
+        if (len(statement) == 0):
+            return_value = command()
+        else:
+            return_value = command(statement, params)
+    except MySQLdb.Error as e:
+        try:
+            print("MySQL Error [{:d}]: {:s}".format(e.args[0], e.args[1]))
+        except IndexError:
+            print("MySQL Error: {:s}".format(str(e)))
+
+    return return_value
+
+
+"""
+End of important functions
+"""
+
+# Script should only execute if not imported
+if __name__ == "__main__":
+    if (len(sys.argv) < 5):
+        print("\nUsage: {} <mysql username> <mysql password> \
+<database name> <state name>".format(
+            sys.argv[0]))
+        exit()
+
+    """
+    IMPORTANT DECLARATIONS
+    """
+
+    MY_HOST = 'localhost'
+    MY_PORT = 3306
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+    STATE_MATCH = sys.argv[4]
+
+    # Create a connection with the database
+    db = MySQLdb.connect(
+            host=MY_HOST,
+            port=MY_PORT,
+            user=MY_USER,
+            passwd=MY_PASS,
+            database=MY_DB)
+
+    # Create a cursor object for interacting with the database
+    cur = db.cursor()
+
+    Run(cur.execute, """SELECT cities.name FROM cities
+    WHERE state_id = (
+        SELECT states.id FROM states
+        WHERE states.name = %s)
+    ORDER BY cities.id ASC;
+    COMMIT""", (STATE_MATCH,))
+
+    # Fetch all results
+    rows = Run(cur.fetchall)
+
+    if rows != ():
+        for row in range(len(rows)):
+            if row != len(rows) - 1:
+                print("{}".format(rows[row][0]), end=", ")
+            else:
+                print("{}".format(rows[row][0]))
+    else:
+        print()
+
+    """
+    CLEAN UP ACTIONS
+    """
+
+    # Close all instances of cursor
+    cur.close()
+
+    # Close all instances of database connection
+    db.close()
--- 0x0F-python-object_relational_mapping/README.md
+++ 0x0F-python-object_relational_mapping/README.md
@@ -253,6 +253,54 @@ guillaume@ubuntu:~/0x0F$
 ```
 **No test cases needed**
 
+## 5. All cities by state
 
+Write a script that takes in the name of a state as an argument and lists all `cities` of that state, using the database `hbtn_0e_4_usa`
+
+* Your script should take 4 arguments: `mysql username`, `mysql password`, `database name` and `state name` (SQL injection free!)
+* You must use the module `MySQLdb` (`import MySQLdb`)
+* Your script should connect to a MySQL server running on `localhost` at port `3306`
+* Results must be sorted in ascending order by `cities.id`
+* You can use only `execute()` once
+* The results must be displayed as they are in the example below
+* Your code should not be executed when imported
+
+```
+guillaume@ubuntu:~/0x0F$ cat 4-cities_by_state.sql
+-- Create states table in hbtn_0e_4_usa with some data
+CREATE DATABASE IF NOT EXISTS hbtn_0e_4_usa;
+USE hbtn_0e_4_usa;
+CREATE TABLE IF NOT EXISTS states ( 
+    id INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+
+CREATE TABLE IF NOT EXISTS cities ( 
+    id INT NOT NULL AUTO_INCREMENT, 
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY(state_id) REFERENCES states(id)
+);
+INSERT INTO cities (state_id, name) VALUES (1, "San Francisco"), (1, "San Jose"), (1, "Los Angeles"), (1, "Fremont"), (1, "Livermore");
+INSERT INTO cities (state_id, name) VALUES (2, "Page"), (2, "Phoenix");
+INSERT INTO cities (state_id, name) VALUES (3, "Dallas"), (3, "Houston"), (3, "Austin");
+INSERT INTO cities (state_id, name) VALUES (4, "New York");
+INSERT INTO cities (state_id, name) VALUES (5, "Las Vegas"), (5, "Reno"), (5, "Henderson"), (5, "Carson City");
+
+guillaume@ubuntu:~/0x0F$ ./5-filter_cities.py root root hbtn_0e_4_usa Texas
+
+guillaume@ubuntu:~/0x0F$ cat 4-cities_by_state.sql | mysql -uroot -p
+Enter password: 
+guillaume@ubuntu:~/0x0F$ ./5-filter_cities.py root root hbtn_0e_4_usa Texas
+Dallas, Houston, Austin
+guillaume@ubuntu:~/0x0F$ ./5-filter_cities.py root root hbtn_0e_4_usa Hawaii
+
+guillaume@ubuntu:~/0x0F$
+```
+
+---
 
 > Onwuta Ebube Gideon
