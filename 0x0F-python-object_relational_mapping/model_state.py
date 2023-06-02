6686e1dac7d1e7ca4d8fa7518fda7b2d1244c055
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-06-02 17:21:22 +0000

    Reversed modifications

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -25,6 +25,5 @@ class State(Base):
     """
 
     __tablename__ = 'states'
-    id = Column(Integer, Sequence('state_id_seq'), unique=True,
-                nullable=False, primary_key=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
