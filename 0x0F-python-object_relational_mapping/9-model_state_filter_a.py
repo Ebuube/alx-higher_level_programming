91bc6fc5e6fd67c3423bf1693b85370395c43862
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-06-02 21:42:03 +0000

    Solved task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This script prints the `State` objects that contain the letter `a` from the
+database `hbtn_0e_6_usa
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+
+    # Verify correct number of arguments
+    if (len(sys.argv) < 4):
+        print("\nUsage: {} <mysql username> <mysql password>\
+<database name>".format(sys.argv[0]))
+        exit(1)
+
+    # Assign arguments
+    MY_USER = sys.argv[1]   # User name
+    MY_PASS = sys.argv[2]   # User password
+    MY_DB = sys.argv[3]     # Database name
+
+    # create engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        MY_USER, MY_PASS, MY_DB), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()     # instance of a session for conversing
+
+    """
+    Print all `State` objects that contain the letter `a`
+    """
+    result = session.query(State).filter(
+            State.name.like('%a%')).order_by(State.id).all()
+
+    if (result is None) or (len(result) == 0):
+        print("Nothing")
+    else:
+        for instance in result:
+            print("{:d}: {:s}".format(instance.id, instance.name))
--- 0x0F-python-object_relational_mapping/README.md
+++ 0x0F-python-object_relational_mapping/README.md
@@ -389,6 +389,19 @@ Write a script that prints the first `State` object from the database `hbtn_0e_6
 * If the table `states` is empty, print `Nothing` followed by a new line
 * Your code should not be executed when imported
 
+## 9. Contains `a`
+
+Write a script that lists all `State` objects that contain the letter `a` from the database `hbtn_0e_6_usa`
+
+* Your script should take 3 arguments: `mysql username`, `mysql password` and `database name`
+* You must use the module `SQLAlchemy`
+* You must import `State` and `Base` from `model_state` - `from model_state import Base, State`
+* Your script should connect to a MySQL server running on `localhost` at port `3306`
+* Results must be sorted in ascending order by `states.id`
+* The results must be displayed as they are in the example below
+* Your code should not be executed when imported
+
+
 
 ---
 
