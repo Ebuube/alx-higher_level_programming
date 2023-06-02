43addcf7d11102af24572f0a57745875d9a9c8a9
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-06-02 20:24:14 +0000

    Solved task 8 and made codebase pycodestyle compliant

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+This script prints the first `State` object from the database `hbtn_0e_6_usa`
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+
+    # Verify correct number of arguments
+    if (len(sys.argv) < 4):
+        print("\nUsage: {} <mysql username> <mysql password>\
+<database name>".format(sys.argv[0]))
+        exit(1)
+
+    # Assign arguments
+    MY_USER = sys.argv[1]   # User name
+    MY_PASS = sys.argv[2]   # User password
+    MY_DB = sys.argv[3]     # Database name
+
+    # create engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        MY_USER, MY_PASS, MY_DB), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()     # instance of a session for conversing
+
+    """
+    for instance in session.query(State).order_by(State.id).all():
+        print("{:d}: {:s}".format(instance.id, instance.name))
+    """
+
+    """
+    Print the first `State` object from the database
+    """
+    result = session.query(State).order_by(State.id)
+
+    if (result is not None) or (len(result) != 0):
+        print("{:d}: {:s}".format(result[0].id, result[0].name))
+    else:
+        print("Nothing")
--- 0x0F-python-object_relational_mapping/README.md
+++ 0x0F-python-object_relational_mapping/README.md
@@ -375,6 +375,20 @@ guillaume@ubuntu:~/0x0F$
 ```
 **No test cases needed**
 
+## 8. First state
+
+Write a script that prints the first `State` object from the database `hbtn_0e_6_usa`
+
+* Your script should take 3 arguments: `mysql username`, `mysql password` and `database name`
+* You must use the module `SQLAlchemy`
+* You must import `State` and `Base` from `model_state` - `from model_state import Base, State`
+* Your script should connect to a MySQL server running on `localhost` at port `3306`
+* The state you display must be the first in `states.id`
+* You are not allowed to fetch all states from the database before displaying the result
+* The results must be displayed as they are in the example below
+* If the table `states` is empty, print `Nothing` followed by a new line
+* Your code should not be executed when imported
+
 
 ---
 
