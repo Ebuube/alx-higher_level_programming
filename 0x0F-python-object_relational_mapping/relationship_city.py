d17abba651bbb3a0eb83aee7060e524c6179237d
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-06-03 19:08:13 +0000

    Created import for  in module relationship_city

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -13,12 +13,14 @@ class City(Base):
     Define the City model which represents an entity in the database.
     It defines a table named `cities`
 
-    @ id (sqlalchemy.Integer):    a column of an auto-generated, unique
+    id (sqlalchemy.Integer):    a column of an auto-generated, unique
         integer, can't be null and is a primary key
-    @ name (sqlalchemy.String):   a column of a string with maximum of 128
+    name (sqlalchemy.String):   a column of a string with maximum of 128
         characters and can't be null
-    @ state_id (sqlalchemy.Integer):    a column of an integer, can't be null
+    state_id (sqlalchemy.Integer):    a column of an integer, can't be null
         and is a foreign key to `states.id`
+    state (relationship with State object): this links the state object
+        that has this city
     """
 
     __tablename__ = 'cities'
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -6,6 +6,7 @@ This module defines the First State model
 import sys
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.orm import relationship
 from sqlalchemy import (create_engine)
 
 my_metadata = MetaData()
@@ -18,11 +19,11 @@ class State(Base):
     Define the State model which represents an entity in the database.
     It defines a column named `states`.
 
-    @ id (sqlalchemy.Integer):    a column of an auto-generated, unique,
+    id (sqlalchemy.Integer):    a column of an auto-generated, unique,
         integer, can't be null and is a primary key
-    @ name (sqlalchemy.String):   a column of a string with maximum 128
+    name (sqlalchemy.String):   a column of a string with maximum 128
         characters and can't be null
-    @ cities (list of City objects):    a relationsip with the class `City`
+    cities (list of City objects):    a relationsip with the class `City`
         If the `State` object is deleted, all linked `City` objects must be
         automatically deleted.
     """
