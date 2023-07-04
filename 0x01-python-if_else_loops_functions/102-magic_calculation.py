c193eba4f0d38adc222197fcdf104d16b7fb7890
Ebuube <onwutaebubegideon1555@gmail.com>
2023-07-04 14:00:49 +0000

    Corrected colution to magic_calculation

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,38 +1,9 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
-    if (a > b) == False:
-        if (c > b) == False:
+    if (a < b) is False:
+        if (c > b) is False:
             return (a * b) - c
         else:
             return a + b
     else:
         return c
-
-"""
-Byte code interpreted
-
- 3           0 LOAD_FAST                0 (a)
-              3 LOAD_FAST                1 (b)
-              6 COMPARE_OP               0 (<)
-              9 POP_JUMP_IF_FALSE       16
-
-  4          12 LOAD_FAST                2 (c)
-             15 RETURN_VALUE
-
-  5     >>   16 LOAD_FAST                2 (c)
-             19 LOAD_FAST                1 (b)
-             22 COMPARE_OP               4 (>)
-             25 POP_JUMP_IF_FALSE       36
-
-  6          28 LOAD_FAST                0 (a)
-             31 LOAD_FAST                1 (b)
-             34 BINARY_ADD
-             35 RETURN_VALUE
-
-  7     >>   36 LOAD_FAST                0 (a)
-             39 LOAD_FAST                1 (b)
-             42 BINARY_MULTIPLY
-             43 LOAD_FAST                2 (c)
-             46 BINARY_SUBTRACT
-             47 RETURN_VALUE
-"""
