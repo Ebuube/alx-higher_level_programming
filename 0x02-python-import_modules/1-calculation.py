7d391df245836858e0b53c5d69930ca40324d342
Ebuube <onwutaebubegideon1555@gmail.com>
2023-07-05 03:32:16 +0000

    Update codebase 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,12 +3,7 @@
 A program that imports functions from the file calculator_1.py
 and does some maths and prints the result.
 """
-
-
 if __name__ == '__main__':
-    """
-    Perform calculations
-    """
     from calculator_1 import (sub, add, mul, div)
 
     a = 10
@@ -17,4 +12,4 @@ if __name__ == '__main__':
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} / {:d} = {:g}".format(a, b, (div(a, b))))
+    print("{:d} / {:d} = {:g}".format(a, b, div(a, b)))
