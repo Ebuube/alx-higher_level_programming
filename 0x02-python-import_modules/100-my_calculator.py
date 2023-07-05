439e89d8ef1e7abd2a475d80e096d5311d295b5d
Ebuube <onwutaebubegideon1555@gmail.com>
2023-07-05 15:41:43 +0000

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,40 +5,21 @@ A program that imports all functions a file and handles basic operations.
 
 
 if __name__ == '__main__':
-    from sys import argv, exit
+    import sys
     from calculator_1 import add, sub, mul, div
 
+    if (len(sys.argv) != 4):
+        print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
+        sys.exit(1)
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        operator = sys.argv[2]
+        operators = {"+": add, "-": sub, "*": div, "/": div}
 
-    if (len(argv) != 4):
-        print("Usage: {} <a> <operator> <b>".format(argv[0]))
-        exit(1)
-
-    operators = ['+', '-', '*', '/']
-    a = int()
-    b = int()
-    result = 0
-
-    # check if the input operator is valid
-    if argv[2] not in operators:
-        print("Unknown operator. Only: are +, -, * and / available")
-        exit(1)
-
-    try:    # reject invalid operands
-        a = int(argv[1])
-        b = int(argv[3])
-    except ValueError:
-        print("Invalid operands")
-        exit(1)
-
-    # get operator
-    if (argv[2] == '+'):
-        calc = add
-    if (argv[2] == '-'):
-        calc = sub
-    if (argv[3] == '*'):
-        calc = mul
-    if (argv[2] == '/'):
-        calc = div
-
-    result = calc(a, b)
-    print("{:d} {:s} {:d} = {}".format(a, argv[2], b, result))
+        if operator in operators.keys():
+            calc = operators[operator]
+            print("{} {} {} = {}".format(a, operator, b, calc(a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
