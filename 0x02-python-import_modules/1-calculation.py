49c875e5d7ec899a6358336c1368971449696f8c
Ebuube <onwutaebubegideon1555@gmail.com>
2023-07-05 10:48:42 +0000

    Removed format specifiers

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""
-A program that imports functions from the file calculator_1.py
-and does some maths and prints the result.
-"""
 if __name__ == '__main__':
     from calculator_1 import (sub, add, mul, div)
-
+    """
+    A program that imports functions from the file calculator_1.py
+    and does some maths and prints the result.
+    """
     a = 10
     b = 5
 
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} / {:d} = {:g}".format(a, b, div(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
