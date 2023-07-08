5cb1977f773577c0745613b001ef710e5c5d4583
Ebuube <onwutaebubegideon1555@gmail.com>
2023-07-08 11:46:29 +0000

    Task 16: CPython #1: PyBytesObject

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,112 +1,86 @@
-#ifndef _MY_PY_INFO
-#define _MY_PY_INFO
-
-
 #include <stdio.h>
+#include "Python.h"
 #include <string.h>
 
-#include "Python.h"
-
-
-/**
- * print_python_bytes - A function that prints some information
- * about a PyBytesObject
- * @p: a PyObject
- *
- * Description: Print format ->
- *
- * *********************** Separtor *****************************************
- * [.] bytes object info
- *   size: 5
- *   trying string: <the object displayed as string>
- *   first <x> bytes: <the first x bytes in hex format, separated by space>
- * *********************** Separtor *****************************************
- *
- * Where x is the number of bytes in the string and the max val of x is 10
- *
- * Note Note Note Note Note
- * If the object p is not a PyBytesObject, print this error message
- * Print format ->
- *
- * *********************** Separtor *****************************************
- * [.] bytes object info
- *   [ERROR] Invalid Bytes Object
- * *********************** Separtor *****************************************
- *
- * Return: nothing
- */
-void print_python_bytes(PyObject *p)
-{
-	char *string = 0;
-	const Py_ssize_t _MAX_ = 10;
-	Py_ssize_t len = 0;
-	Py_ssize_t count = 0;
-
-	printf("[.] bytes object info\n");	/* for all objects */
-	if (!PyBytes_Check(p))
-	{
-		printf("  [ERROR] Invalid Bytes Object\n");
-		return;
-	}
-	len = ((PyBytesObject *)p)->ob_base.ob_size;
-	string = ((PyBytesObject *)p)->ob_sval;
-
-	printf("  size = %ld\n", len);
-	printf("  trying string: %s\n", string);
-
-	len = (len < _MAX_) ? len + 1 : _MAX_;
-	printf("  first %ld bytes:", len);
-	for (count = 0; count < len; count++)
-	{
-		string[count] = (string[count] < 0) ?
-				string[count] + 256 : string[count];
-		printf(" %02x", string[count]);
-	}
-	putchar('\n');
-}
-
+/* FUNCTION PROTOTYPES */
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
 
 /**
- * print_python_list - prints some information about a python list object
- * @p: a python list object
+ * print_python_list - print basic information about Python lists
+ * and Python bytes objects
+ * @p: a list object
  *
- * Description: Print format->
- *
- * *********************** Separtor *****************************************
- * [*] Size of the Python List = <number of current elements>
- * [*] Allocated = <number of allocated>
- * Element 0: <type name>
- * Element 1: <type name>
- * Element (n - 1): <type name>
- * *********************** Separtor *****************************************
- *
- * Note Note that for the element types, it will be printed for only a list
- * with length n, such that n > 0
+ * Description: Information to be printed
+ * [*] Python list info
+ * [*] Size of the Python List = %ld
+ * [*] Allocated = %ld
+ * Element %d: tmp_name # where %d is the index of the element not value
  *
  * Return: nothing
  */
 void print_python_list(PyObject *p)
 {
-	PyListObject *list = 0;
-	char *type_name = 0;
-	ssize_t count = 0;
+	PyListObject *mylist = NULL;
+	ssize_t i = 0;
 
-	if (!p)
-	{
-		return;
-	}
-	list = (PyListObject *)p;	/* Important for allocated and size */
-	printf("[*] Python List info\n");	/* for all PyListObject's */
-	printf("[*] Size of the Python List = %ld\n", list->ob_base.ob_size);
-	printf("[*] Allocated = %ld\n", list->allocated);
+	mylist = (PyListObject *)p;
 
-	for (count = 0; count < list->ob_base.ob_size; count++)
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", mylist->ob_base.ob_size);
+	printf("[*] Allocated = %ld\n", mylist->allocated);
+
+	/* All elements of the list */
+	for (i = 0; i < mylist->ob_base.ob_size; i++)
 	{
-		type_name = (char *)list->ob_item[count]->ob_type->tp_name;
-		printf("Element %ld: %s\n", count, type_name);
-		if (PyBytes_Check(list->ob_item[count]))
-			print_python_bytes(list->ob_item[count]);
+		printf("Element %ld:", i);
+		printf(" %s\n", (char *)mylist->ob_item[i]->ob_type->tp_name);
+
+		if (strcmp(mylist->ob_item[i]->ob_type->tp_name, "bytes") == 0)
+		{
+			print_python_bytes(mylist->ob_item[i]);
+		}
 	}
 }
 
-#endif	/* _MY_PY_INFO */
+
+/**
+ * print_python_bytes - print basic information about Python bytes
+ * @p: a PyBytesObject
+ *
+ * Description: Information to be printed
+ * [.] bytes object info
+ *	size: %ld
+ *	trying string: %s
+ *	first n bytes: 0x0b 0x0b 0x0b 0x0b # Where n is string len, max n is 10
+ *
+ * Return: nothing
+ */
+void print_python_bytes(PyObject *p)
+{
+	PyBytesObject *bytes;
+	ssize_t i = 0;
+	ssize_t size = 0;
+	const ssize_t MAX = 10;	/* maximum bytes to print */
+
+	printf("[.] bytes object info\n");
+
+	if ((strcmp(p->ob_type->tp_name, "bytes") != 0))
+	{/* Not a PyBytesObject */
+		printf("\t[ERROR] Invalid Bytes Object\n");
+		return;
+	}
+
+	bytes = (PyBytesObject *)p;
+
+	size = bytes->ob_base.ob_size;
+	printf("\tsize: %ld\n", size);
+	printf("\ttrying string: %s\n", bytes->ob_sval);
+
+	printf("first %ld bytes:", ((size > MAX) ? MAX : size + 1));
+	for (i = 0; i < MAX && i < size + 1; i++)
+	{
+		printf(" %02x", bytes->ob_sval[i]);
+	}
+	printf("\n");
+}
