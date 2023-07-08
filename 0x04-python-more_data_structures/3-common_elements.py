6947b364a652614d283efe9cf8bf02bafa53fede
Ebuube <onwutaebubegideon1555@gmail.com>
2023-07-08 01:32:31 +0000

    modified 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -8,11 +8,12 @@ def common_elements(set_1, set_2):
         set_2: the second set of data
 
     Returns: a set of the common elements in set_1 and set_2
+            else return an empty list
     """
     if not set_1 or len(set_1) == 0:
-        return set_2
+        return []
     if not set_2 or len(set_2) == 0:
-        return set_1
+        return []
 
     set_3 = set(set_1) & set(set_2)
     return (set_3)
