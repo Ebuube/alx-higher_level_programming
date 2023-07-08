bdac6cf60b420c41e175df3eb8f784d21cb161ea
Ebuube <onwutaebubegideon1555@gmail.com>
2023-07-08 01:50:02 +0000

    Task 11: Multiply by using map

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,16 +1,3 @@
 #!/usr/bin/python3
-
 def multiply_list_map(my_list=[], number=0):
-    """A function that returns a list with all values multiplied by a number
-    without using any loops.
-    Args:
-        my_list = a list variable
-        number = a numerical variable
-    Returns:
-        a list with the values multiplied by a number
-    """
-    if not my_list or len(my_list) == 0:
-        return None
-
-    new_list = list(map((lambda element: element * number), my_list))
-    return (new_list)
+    return list(map((lambda x: x * number), my_list))
