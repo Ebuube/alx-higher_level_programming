4fb32bf567d24df2cad2942226932c4c0c38812b
Ebuube <onwutaebubegideon1555@gmail.com>
2023-07-10 17:49:17 +0000

    Task 9: Full rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This module defines the class `Rectangle`, subclass of `BaseGeometry`
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Definition of the class
+    """
+    def __init__(self, width, height):
+        """
+        Initialization of instance
+        """
+        args = {'width': width, 'height': height}
+        for key, val in args.items():
+            self.integer_validator(key, val)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Return the product of the `width` and `height` of the instance
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Return the non-canonical string representation of instance
+        """
+        val = "[{}] {}/{}".format(type(self).__name__,
+                                  self.__width, self.__height)
+        return val
