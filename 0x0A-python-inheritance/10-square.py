8f05794567af3cf4825206eb9ef74c96bfea68e8
Ebuube <onwutaebubegideon1555@gmail.com>
2023-07-10 18:06:53 +0000

    Update codebase: Square

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -15,8 +15,9 @@ class Square(Rectangle):
         """
         Initialization of instance
         """
-        super().__init__(size, size)
+        self.integer_validator('size', size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         """
