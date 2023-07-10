2eb346ffeab3d897eb0ddb2d6a8baa2c91c7f42a
Ebuube <onwutaebubegideon1555@gmail.com>
2023-07-10 16:52:21 +0000

    Task 6: Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module defines the class `BaseGeometry`
+"""
+
+
+class BaseGeometry:
+    """
+    Definition of the class
+    """
+    def area(self):
+        """
+        Raise an exception
+        """
+        raise Exception("area() is not implemented")
