7f35e370e2c579e7603f67b91f1698d08822d598
Ebuube <onwutaebubegideon1555@gmail.com>
2023-07-11 17:01:57 +0000

    Task 0: Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module implements the read_file function
+"""
+
+
+def read_file(filename=""):
+    """
+    Read a text file and print it to stdout
+    """
+    mode = 'r'
+    enc = "utf-8"
+    with open(filename, mode, encoding=enc) as f:
+        print(f.read())
