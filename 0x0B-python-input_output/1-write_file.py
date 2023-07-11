398a1cf8c02323b4dd17f9a2aa28f1b0a5bcf0b2
Ebuube <onwutaebubegideon1555@gmail.com>
2023-07-11 17:30:28 +0000

    Updated codebase

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,10 +8,13 @@ def write_file(filename="", text=""):
     """
     Write a string to a text file (UTF8)
     Return: the number of characters written
+        0 if nothing was written
     """
     mode = 'w'      # write mode
     enc = "utf-8"   # UTF8 encoding
     num_chars = 0
-    with open(filename, mode, encoding=enc) as f:
-        num_chars = f.write(text)
-        return num_chars
+    if filename != "":
+        with open(filename, mode, encoding=enc) as f:
+            num_chars = f.write(text)
+
+    return num_chars
