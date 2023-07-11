7457e57a2fba4bd3247788ba92b96952806ef134
Ebuube <onwutaebubegideon1555@gmail.com>
2023-07-11 18:37:11 +0000

    Task 7: Load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module addss all arguments to a Python list, and then save them to a file
+"""
+import sys
+import os
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+if __name__ == "__main__":
+    filename = "./add_item.json"      # file to save objects
+    enc = "utf-8"       # Encoding to use
+    my_list = list()
+
+    if len(sys.argv) != 0 and os.path.exists(filename):
+        # Feed arguments to list
+        my_list = load_from_json_file(filename)
+        for i in range(1, len(sys.argv)):
+            my_list.append(sys.argv[i])
+
+    # Save object
+    save_to_json_file(my_list, filename)
