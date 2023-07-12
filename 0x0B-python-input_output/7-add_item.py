7bfb3c40a4bb3f06fedf23167f0b2528eb51f78b
Ebuube <onwutaebubegideon1555@gmail.com>
2023-07-12 13:57:17 +0000

    Case without file and add one item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module adds all arguments to a Python list, and then save them to a file
+This module addss all arguments to a Python list, and then save them to a file
 """
 import sys
 import os
@@ -10,15 +10,17 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-filename = "./add_item.json"      # file to save objects
-enc = "utf-8"       # Encoding to use
-my_list = list()
+if __name__ == "__main__":
+    filename = "./add_item.json"      # file to save objects
+    enc = "utf-8"       # Encoding to use
+    my_list = list()
 
-if len(sys.argv) > 1 and os.path.exists(filename):
-    # Feed arguments to list
-    my_list = list(load_from_json_file(filename))
+    if os.path.exists(filename):
+        # Load saved objects to memory
+        my_list = load_from_json_file(filename)
+
     for i in range(1, len(sys.argv)):
         my_list.append(sys.argv[i])
 
-# Save object
-save_to_json_file(my_list, filename)
+    # Save object
+    save_to_json_file(my_list, filename)
