0355cc847114221eded15df79974196515e8e9aa
Ebuube <onwutaebubegideon1555@gmail.com>
2023-07-14 16:13:09 +0000

    Removed unwanted tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -10,7 +10,7 @@ import re
 from models.base import Base
 
 
-class test_base(unittest.TestCase):
+class test_Base(unittest.TestCase):
     """
     Definition of test for the class
     """
@@ -24,20 +24,6 @@ class test_base(unittest.TestCase):
         super().__init__(*args, **kwargs)
         self.value = Base
         self.name = self.value.__name__
-
-        # obtain source code
-        delim = '.'
-        self._sep = '/'
-        fmt = ".py"
-        # pattern : <class 'models.base.Base'>
-        pattern = ".*\'(.*\.*.*\.*.*)\'.*"
-        result = re.search(pattern, str(self.value))
-        track = result.group(1)
-        track = (track.split(delim))[:-1]
-        track = self._sep.join(track)
-        track = track + fmt
-        if os.path.exists(track):
-            self._source_code = track
 
     def setUp(self):
         """
@@ -62,45 +48,3 @@ class test_base(unittest.TestCase):
 
         for file_path in files:
             os.remove(file_path)
-
-    def test_package(self):
-        """
-        Ensure that ``__init__.py`` file is present in `models` directory
-        """
-        package = "__init__.py"
-        path = ((self._source_code.split(self._sep)[0]) + self._sep +
-                "{}".format(package))
-        self.assertTrue(os.path.exists(path))
-
-    def test_pep8(self):
-        """
-        Ensure that source code is Pep8 or pycodestyle compliant
-        """
-        style = pep8.StyleGuide(quite=False)
-        p = style.check_files([self._source_code])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
-
-    def test_docstrings(self):
-        """
-        Ensure that the module and its contents have docstrings
-        """
-        # module = (self._source_code.split(self._sep))[-1]
-        # to be completed
-        pass
-
-    def test_executable_file(self):
-        """
-        Ensure that the source code has executable access for the users
-        """
-
-        """Check for read access"""
-        is_read_true = os.access(self._source_code, os.R_OK)
-        self.assertTrue(is_read_true)
-
-        """Check for write access"""
-        is_write_true = os.access(self._source_code, os.W_OK)
-        self.assertTrue(is_write_true)
-
-        """Check for execution access"""
-        is_exec_true = os.access(self._source_code, os.X_OK)
-        self.assertTrue(is_exec_true)
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Test for the ``Rectangle`` class
+"""
+from tests.test_models.test_base import test_Base
+from models.rectangle import Rectangle
+
+
+class test_Rectangle(test_Base):
+    """
+    Define extra tests for the ``Rectangle`` class
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        Initialization of the class' test
+        """
+        super().__init__(*args, **kwargs)
+        self.value = Rectangle
+        self.name = self.value.__name__
+
+    def setUp(self):
+        """
+        Set up for tests
+        """
+        super().setUp()
+
+    def tearDown(self):
+        """
+        Tear down for the tests
+        """
+        super().tearDown()
+
+    def test_attrs(self):
+        """
+        Ensure that attributes are complete
+        """
+        new = self.value(12, 3, 4, 6)
+        attrs = ["id", "width", "height", "x", "y"]
+        for attr in attrs:
+            with self.subTest(attr=attr):
+                self.assertTrue(hasattr(new, attr))
