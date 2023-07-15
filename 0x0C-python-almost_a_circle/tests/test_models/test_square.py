a7b92c3959d55e04055286123180fcf6b954d51c
Ebuube <onwutaebubegideon1555@gmail.com>
2023-07-15 20:16:40 +0000

    Tests 11: Square size -> getter and setter

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -89,3 +89,24 @@ class test_Square(test_Rectangle):
         string = string.format(type(new).__name__, new.id, new.x, new.y,
                                new.size)
         self.assertEqual(string, str(new))
+
+    def test_setter_getter_attrs(self):
+        """
+        Ensure ``Square`` has setter and getter methods for ``size`` attribute
+
+        To check for getter -> fget
+        >>> Square.size.fget is not None
+
+        To check for setter -> fset
+        >>> Square.size.fset is not None
+        """
+        methods = {"fset": "fset", "fget": "fget"}
+        attrs = {"size": "size", "x": "x", "y": "y", "width": "width",
+                 "height": "height"}
+
+        for attr in attrs.keys():
+            for method in methods.keys():
+                with self.subTest(method=method, attr=attr):
+                    self.assertTrue(hasattr(Square, attr))
+                    my_property = getattr(Square, attr)
+                    self.assertTrue(getattr(my_property, method) is not None)
