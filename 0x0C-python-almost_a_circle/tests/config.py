f6ade60010c728947991e2ad9f1f669018546049
Ebuube <onwutaebubegideon1555@gmail.com>
2023-07-15 08:52:41 +0000

    Added more configurations to config.py file

--- 0x0C-python-almost_a_circle/tests/config.py
+++ 0x0C-python-almost_a_circle/tests/config.py
@@ -4,4 +4,6 @@ Configuration for the project
 """
 
 pep8_checked = False    # If all files have been pep8 checked
-pep8_verbose = False     # Print files checked/verbosity
+pep8_verbose = True    # Print files checked/verbosity
+setUp_verbose = True    # Specify what happens during setUp
+tearDown_verbose = True     # Specify what happends during tearDown
