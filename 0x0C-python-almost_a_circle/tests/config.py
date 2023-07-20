4568eab5cc07cb2f8d136e9278c6bb212c10c263
Ebuube <onwutaebubegideon1555@gmail.com>
2023-07-20 17:44:43 +0000

    Updated cofig

--- 0x0C-python-almost_a_circle/tests/config.py
+++ 0x0C-python-almost_a_circle/tests/config.py
@@ -3,7 +3,7 @@
 Configuration for the project
 """
 
-pep8_checked = False    # Default = False; If all files have been pep8 checked
+pep8_checked = False    # Default = False; If all files need to be checked
 pep8_verbose = False    # Print files checked/verbosity
 setUp_verbose = False   # Specify what happens during setUp
 tearDown_verbose = False    # Specify what happens during tearDown
