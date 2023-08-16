d0eb0ef342dd0c52cfecf4d9a162121645e26729
Ebuube <onwutaebubegideon1555@gmail.com>
2023-08-16 23:47:45 +0000

    17. Go to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,16 @@
+-- Conver the hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+-- convert all of the following to UTF8:
+-- *	Database	`hbtn_0c_0`
+-- *	Table		`first_table`
+-- *	Field		`first_table`
+
+-- Alter the database
+ALTER DATABASE `hbtn_0c_0` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci';
+
+-- Alter the table
+USE `hbtn_0c_0`;
+ALTER TABLE `first_table` CONVERT TO CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci';
+
+-- Alter a field
+ALTER TABLE `first_table`
+CHANGE `name` `name` VARCHAR(256) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci';
