47660fdf11b920031f9c3d5f6fb23ab34c8e3c52
Ebuube <onwutaebubegideon1555@gmail.com>
2023-08-16 23:59:14 +0000

    17. correction

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -9,8 +9,8 @@ ALTER DATABASE `hbtn_0c_0` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci';
 
 -- Alter the table
 USE `hbtn_0c_0`;
-ALTER TABLE `first_table` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci';
+ALTER TABLE `first_table` CONVERT TO CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci';
 
 -- Alter a field
-ALTER TABLE `first_table`
-CHANGE `name` `name` VARCHAR(256) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci';
+-- ALTER TABLE `first_table`
+-- CHANGE `name` `name` VARCHAR(256) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci';
