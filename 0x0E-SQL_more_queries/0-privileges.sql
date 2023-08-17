2653c01bd20b89104ec01bc58afb5b4f99a67623
Ebuube <onwutaebubegideon1555@gmail.com>
2023-08-17 21:42:21 +0000

    0 - update 4

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- List all privileges of the MySQL users 'user_0d_1' and 'user_0d_2' on localhost
+-- List all privileges of the MySQL user 'user_0d_1'
 SHOW GRANTS FOR user_0d_1@localhost;
+-- List all privileges of the MySQL user 'user_0d_2'
 SHOW GRANTS FOR user_0d_2@localhost;
