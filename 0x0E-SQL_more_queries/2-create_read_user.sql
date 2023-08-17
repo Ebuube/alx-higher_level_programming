21d20637d53dfc43ae240f365d2b0ccb99a998be
Ebuube <onwutaebubegideon1555@gmail.com>
2023-08-17 22:06:48 +0000

    2 - update 1

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,5 +2,5 @@
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 GRANT SELECT
-ON `hbtn_0d_2`.*
+ON hbtn_0d_2.*
 TO 'user_0d_2_pwd';
