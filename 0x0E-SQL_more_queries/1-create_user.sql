b5b9b7cc289572595eb9888e88fe71c90a89d3f7
Ebuube <onwutaebubegideon1555@gmail.com>
2023-08-17 21:56:43 +0000

    1 - update 3

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,3 @@
 -- Create the MySQL user 'user_0d_1'
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant privileges
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Flush privileges
-FLUSH PRIVILEGES;
