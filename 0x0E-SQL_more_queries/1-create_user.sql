4a6f17fc4ddc4787b4bc17209f2e45d6950d5ff0
Ebuube <onwutaebubegideon1555@gmail.com>
2023-08-17 21:49:05 +0000

    1 - update 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,3 +3,6 @@ CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant privileges
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Flush privileges
+FLUSH PRIVILEGES;
