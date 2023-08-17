23daef05bdabdf235bd209ec925ceaa626bede65
Ebuube <onwutaebubegideon1555@gmail.com>
2023-08-17 21:46:22 +0000

    1. Root user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Create the MySQL user 'user_0d_1'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant privileges
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
