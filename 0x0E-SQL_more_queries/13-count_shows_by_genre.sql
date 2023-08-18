4a873e533668700dc5223d7707467d1bbf1764d1
Ebuube <onwutaebubegideon1555@gmail.com>
2023-08-18 00:56:02 +0000

    13 - updated JOIN

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 -- Don't display a genre that doesn't have any shows linked
 SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres
-LEFT JOIN tv_show_genres
+INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
