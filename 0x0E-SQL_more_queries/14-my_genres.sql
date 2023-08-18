3dabd4c3a92b059c5aaea1f7da7c265421f963e6
Ebuube <onwutaebubegideon1555@gmail.com>
2023-08-18 09:25:42 +0000

    Included a comment

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,8 @@
+-- List all genres of the show 'Dexter'
 SELECT tv_genres.name
 FROM (tv_shows
 	INNER JOIN tv_show_genres
-	ON tv_shows.id = tv_show_genres.show_id
-	AND tv_shows.title = 'Dexter')
+	ON tv_shows.id = tv_show_genres.show_id AND tv_shows.title = 'Dexter')
 INNER JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY tv_genres.name ASC;
