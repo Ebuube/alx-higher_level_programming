aa6aef3e0a5a68c5e031042cd2555e12c2664216
Ebuube <onwutaebubegideon1555@gmail.com>
2023-09-12 00:42:56 +0000

    Task 7. I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+// I love C
+
+const args = process.argv.slice(2);
+// arg[0] => interpreter
+// arg[1] => name of program being executed
+
+const x = parseInt(args[0]);
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences')
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
