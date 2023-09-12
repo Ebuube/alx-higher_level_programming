4f1ed1e56bf985a5776761271c5b18eee87a3dcc
Ebuube <onwutaebubegideon1555@gmail.com>
2023-09-12 00:24:05 +0000

    Task 4. Create a sentence

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// arguments
+
+const cmdLineArgs = process.argv.slice(2);
+// Remove the first two arguments
+// The first argument is the interpreter running the program
+// The second argument is the absolute path of the program being executed
+// The rest are the command line arguments supplied to the program
+
+const myVar = cmdLineArgs[0] + ' is ' + cmdLineArgs[1];
+console.log(myVar);
