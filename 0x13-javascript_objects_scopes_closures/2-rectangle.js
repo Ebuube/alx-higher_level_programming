6dc90fd332b40dddc86f0f548ed1cb7528e07d50
Ebuube <onwutaebubegideon1555@gmail.com>
2023-09-12 13:40:02 +0000

    Task 2. Rectangle #2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// A rectangle class with a constructor that validates type of arguments
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
