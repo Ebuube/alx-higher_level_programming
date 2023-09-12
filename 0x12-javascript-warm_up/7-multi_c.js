cde1f67ff2ad03ed8a6be72a68c9cd3398e9483a
Ebuube <onwutaebubegideon1555@gmail.com>
2023-09-12 00:43:45 +0000

    Corrected semistandard error

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -9,7 +9,7 @@ const args = process.argv.slice(2);
 const x = parseInt(args[0]);
 
 if (isNaN(x)) {
-  console.log('Missing number of occurrences')
+  console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
