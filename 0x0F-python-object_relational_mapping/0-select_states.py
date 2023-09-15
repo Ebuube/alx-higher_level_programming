a5046cfc9873922564765a5155bf8ca8bac56ab6
Ebuube <onwutaebubegideon1555@gmail.com>
2023-09-15 14:00:32 +0000

    TAsk 0. Get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Get all states from a database
+"""
+import MySQLdb
+from utility import execsafe
+from sys import argv
+
+
+# Get database login details from command line arguments
+if len(argv) < 3:
+    msg = "Usage: {:s} mysql username, mysql password, database name"
+    msg.format(argv[0])
+    print(msg)
+    exit(1)
+
+db_login = {
+        "host":     'localhost',
+        "user":     str(argv[1]),
+        "passwd":   str(argv[2]),
+        "db":       str(argv[3]),
+        "port":     3306}
+
+# Connection to database
+db = MySQLdb.connect(**db_login)
+
+# Put a connection to good use
+cur = db.cursor()
+
+# Execute query
+execsafe(cur, "SELECT * FROM states")
+
+# Print details
+rows = cur.fetchall()
+
+if rows is not None:
+    for row in rows:
+        print("{}".format(row))
+
+# Clean up
+cur.close()
+db.close()
---
+++ 0x0F-python-object_relational_mapping/utility.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Function to safely execute a query in MySQLdb
+"""
+import MySQLdb
+
+def execsafe(cur, query):
+    """
+    Safely execute a query on a cursor
+    """
+    if cur is None or query is None:
+        return
+
+    try:
+        cur.execute(query)
+    except MySQLdb.Error as e:
+        try:
+            print("MySQL Error [{:d}]: {}".format(e.args[0], e.args[1]))
+        except IndexError:
+            print("MySQL Error: {}".format(str(e)))
