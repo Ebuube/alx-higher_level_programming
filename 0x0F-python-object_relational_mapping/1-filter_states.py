db5ae61d9c5d701a762e174db7aed15b8d54b06c
Ebuube <onwutaebubegideon1555@gmail.com>
2023-09-15 15:53:32 +0000

    Modules should not be executed on import

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,38 +7,39 @@ from utility import execsafe
 from sys import argv
 
 
-# Get database login details from command line arguments
-if len(argv) < 3:
-    msg = "Usage: {:s} mysql username, mysql password, database name"
-    msg.format(argv[0])
-    print(msg)
-    exit(1)
+if __name__ == '__main__':
+    # Get database login details from command line arguments
+    if len(argv) < 3:
+        msg = "Usage: {:s} mysql username, mysql password, database name"
+        msg.format(argv[0])
+        print(msg)
+        exit(1)
 
-db_login = {
-        "host":     'localhost',
-        "user":     str(argv[1]),
-        "passwd":   str(argv[2]),
-        "db":       str(argv[3]),
-        "port":     3306}
+    db_login = {
+            "host":     'localhost',
+            "user":     str(argv[1]),
+            "passwd":   str(argv[2]),
+            "db":       str(argv[3]),
+            "port":     3306}
 
-# Connection to database
-db = MySQLdb.connect(**db_login)
+    # Connection to database
+    db = MySQLdb.connect(**db_login)
 
-# Put a connection to good use
-cur = db.cursor()
+    # Put a connection to good use
+    cur = db.cursor()
 
-# Execute query
-execsafe(cur, """SELECT * FROM states
-                    WHERE states.name LIKE BINARY 'N%'
-                    ORDER BY states.id ASC""")
+    # Execute query
+    execsafe(cur, """SELECT * FROM states
+                        WHERE states.name LIKE BINARY 'N%'
+                        ORDER BY states.id ASC""")
 
-# Print details
-rows = cur.fetchall()
+    # Print details
+    rows = cur.fetchall()
 
-if rows is not None:
-    for row in rows:
-        print("{}".format(row))
+    if rows is not None:
+        for row in rows:
+            print("{}".format(row))
 
-# Clean up
-cur.close()
-db.close()
+    # Clean up
+    cur.close()
+    db.close()
