c12379ae4e11b3fb2d84b83baa5d2cc9650bfd9d
Ebuube <onwutaebubegideon1555@gmail.com>
2023-09-16 16:34:01 +0000

    Changed the module for Base and State

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -3,8 +3,8 @@
 Cities in state
 """
 from sys import argv
-from model_state import Base, State
-from model_city import City
+from relationship_state import Base, State
+from relationship_city import City
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
