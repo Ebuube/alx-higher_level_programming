bd2d42e3c38e39f3e068ded269029540e197903c
Ebuube <onwutaebubegideon1555@gmail.com>
2023-09-16 15:32:10 +0000

    Task 15: City relationship

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Cities in state
+"""
+from sys import argv
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    # Get database login details from command line arguments
+    if len(argv) < 4:
+        msg = "Usage: {:s} mysql username, mysql password, database name"
+        msg.format(argv[0])
+        print(msg)
+        exit(1)
+
+    # Global scope -> Application starts
+    Session = sessionmaker()
+
+    db_login = {
+            "host":     'localhost',
+            "user":     str(argv[1]),
+            "passwd":   str(argv[2]),
+            "db_name":       str(argv[3]),
+            "port":     3306}
+
+    engine_url = 'mysql+mysqldb://{}:{}@localhost/{}'
+    engine_url = engine_url.format(db_login['user'],
+                                   db_login['passwd'],
+                                   db_login['db_name'])
+
+    # later, in a local scope, create and use a session:
+    engine = create_engine(engine_url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session.configure(bind=engine)
+
+    sess = Session()
+
+    # Create records
+    state_name = "California"
+    new_state = State(name=state_name)
+    sess.add(new_state)
+
+    city_name = "San Francisco"
+    new_city = City(name=city_name, state=new_state)
+    sess.add(new_city)
+
+    # Save changes
+    sess.commit()
+    sess.close()
+    engine.dispose()
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+First state model
+"""
+from sqlalchemy import (Column, Integer, String, MetaData)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+my_metadata = MetaData()
+Base = declarative_base(metadata=my_metadata)
+
+
+class State(Base):
+    """
+    A state model
+
+    table: states
+    id: primary key
+    name: name of state
+    cities: all city objects connected to this state object
+    """
+    __tablename__ = 'states'
+    id = Column("id", Integer, nullable=False, autoincrement=True,
+                primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete")
+    # cities = relationship("City", backref="state" cascade="all, delete")
