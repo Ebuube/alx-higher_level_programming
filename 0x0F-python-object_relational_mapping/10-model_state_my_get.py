63acfe55d4b44bc4b34f77abec2d42a6220e2ce4
Ebuube <onwutaebubegideon1555@gmail.com>
2023-09-16 14:23:35 +0000

    Task 10: Get a state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Contains `a`
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    # Get database login details from command line arguments
+    if len(argv) < 5:
+        msg = "Usage: {:s} mysql username, mysql password, database name"
+        msg.format(argv[0])
+        print(msg)
+        exit(1)
+
+    match = argv[4]
+    # Global scope -> Application starts
+    Session = sessionmaker()
+
+    db_login = {
+            "host":     'localhost',
+            "user":     str(argv[1]),
+            "passwd":   str(argv[2]),
+            "db_name":       str(argv[3]),
+            "port":     3306}
+
+    engine_url = 'mysql+mysqldb://{}:{}@localhost/{}'
+    engine_url = engine_url.format(db_login['user'],
+                                   db_login['passwd'],
+                                   db_login['db_name'])
+
+    # later, in a local scope, create and use a session:
+    engine = create_engine(engine_url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session.configure(bind=engine)
+
+    sess = Session()
+
+    # QUERY
+    res = sess.query(State).filter(
+            State.name.like(match)).order_by(State.id).first()
+
+    if res is not None:
+        print("{:d}".format(res.id))
+    else:
+        print("Not found")
