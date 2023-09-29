959783b434edcb9c4cd7a43abb26bcd60d756d22
Ebuube <onwutaebubegideon1555@gmail.com>
2023-09-29 07:19:57 -0700

    Made program generic

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # show all methods available
-curl -ILsX OPTIONS 0.0.0.0:5000/route_4 | grep "Allow: " | cut --delimiter=" " --fields=2-
+curl -ILsX OPTIONS "$1" | grep "Allow: " | cut --delimiter=" " --fields=2-
