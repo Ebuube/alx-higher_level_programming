37247582d6a5d950126fd7935cd32a7be8aa8401
Ebuube <onwutaebubegideon1555@gmail.com>
2023-09-30 23:42:47 +0000

    Memory efficient

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,6 +5,28 @@ NB: A peak element in a list of integers is any element that is greater
 than both of its neighboring elements
 """
 
+
+def ceil(frac):
+    """
+    Find the lowest number greater than the fraction
+    e.g
+    >>> ceil(5.5)
+    6
+    >>> ceil(5)
+    5
+    """
+    if type(frac) is int:
+        return frac
+    if not isinstance(frac, float):
+        return
+
+    n_int = int(frac)
+    if frac > n_int:
+        return n_int + 1
+    else:
+        return n_int
+
+
 def find_peak(list_of_integers):
     """
     Find a peak in a list of integers
@@ -14,54 +36,20 @@ def find_peak(list_of_integers):
     if len(list_of_integers) == 0:
         return
 
-    # Indices
-    lower = 0;
-    upper = len(list_of_integers) - 1
-    peak = find_peak_aux(list_of_integers, lower, upper)
-    return(peak)
-
-def find_peak_aux(i_list, lower, upper):
-    """
-    An auxiallary function
-    Find any of the peaks in a list
-    """
-    '''
-    # Safety
-    if trials > 10:     # test
-        print("number of trials has reached maximum: {}".format(trials))    # test
-        return
-    else:
-        trials += 1
-    '''
-
-    if i_list is None or type(i_list) is not list:
-        return
-    if len(i_list) == 0:
-        return
-    if upper < 0 or lower < 0 or lower > upper:
-        return
-
-    # Operation
-    if upper == lower:
-        return i_list[upper]
-
-    if upper - lower == 1:
-        if i_list[upper] >= i_list[lower]:
-            return i_list[upper]
-        else:
-            return i_list[lower]
-
-    if upper - lower == 2:
-        mid = lower + 1
-        if (i_list[mid] >= i_list[mid -1 ] and i_list[mid] >= i_list[mid + 1]):
-            return i_list[mid]
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    elif len(list_of_integers) == 2:
+        return max(list_of_integers)
+    elif len(list_of_integers) == 3:
+        if (list_of_integers[1] >= list_of_integers[0] and
+                list_of_integers[1] >= list_of_integers[2]):
+            return list_of_integers[1]
         else:
             return None
-
-    elif upper - lower > 2:
-        peak1 = find_peak_aux(i_list, lower, lower + int((upper - lower) / 2))
-        peak2 = find_peak_aux(i_list, lower + int((upper - lower) / 2) + 1, upper)
-        if peak1 is not None:
-            return peak1
+    else:
+        mid = ceil(len(list_of_integers) / 2)
+        left = find_peak(list_of_integers[:mid])
+        if left is not None:
+            return left
         else:
-            return peak2
+            return find_peak(list_of_integers[mid:])
