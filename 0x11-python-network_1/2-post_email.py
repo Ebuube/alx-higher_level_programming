7e95c7f18fe0cee5a4adc68c5f5dad3abb891e63
Ebuube <onwutaebubegideon1555@gmail.com>
2023-10-02 17:15:32 +0000

    modified modules

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,7 +2,7 @@
 """
 POST an email #0
 """
-import urllib
+from urllib import (parse, request)
 import sys
 
 
@@ -10,9 +10,9 @@ if __name__ == '__main__':
     values = {'email': sys.argv[2]}
     url = sys.argv[1]
 
-    data = urllib.parse.urlencode(values)
+    data = parse.urlencode(values)
     data = data.encode('ascii')     # data should be bytes
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
+    req = request.Request(url, data)
+    with request.urlopen(req) as response:
         msg = response.read()
         print(msg.decode('utf-8'))
