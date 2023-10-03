316fbfc43218d92870a25043d5731f5bdbff87ba
Ebuube <onwutaebubegideon1555@gmail.com>
2023-10-03 23:54:05 +0000

    Adjusted HTTP verb

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,14 +2,14 @@
 """
 Error code #1
 """
-from requests import post
+from requests import get
 from sys import argv
 
 
 if __name__ == "__main__":
     url = argv[1]
 
-    response = post(url)
+    response = get(url)
     if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))
     else:
