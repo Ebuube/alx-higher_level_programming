1493c467c880632862e1b675ef75b165568f7a54
Ebuube <onwutaebubegideon1555@gmail.com>
2023-11-03 01:26:18 +0000

    Semistandard compliant

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,7 +1,5 @@
-$("DIV#red_header").bind("click", function() {
-    if ($("body header").hasClass("red")) {
-        ;
-    } else {
-        $("body header").addClass("red");
-    }
-})
+$('DIV#red_header').bind('click', function () {
+  if (!($('body header').hasClass('red'))) {
+    $('body header').addClass('red');
+  }
+});
