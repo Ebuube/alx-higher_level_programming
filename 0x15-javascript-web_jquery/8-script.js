f3866b2defcfa1aca4c1e87becc06a2385636467
Ebube Gideon Onwuta <onwutaebubegideon1555@gmail.com>
2023-11-03 03:07:23 +0100

    Task 8: Star Wars movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,16 @@
+// Using the core $.ajax() method
+$.ajax({
+    // The URL for the request
+    url: "https://swapi-api.alx-tools.com/api/films/?format=json",
+
+    type: 'GET',
+  
+    dataType: 'json'
+  })
+  
+    .done(function (json) {
+        // List the title for all movies
+        for (film of json.results) {
+            $("UL#list_movies").append(`<li>${film.title}</li>`);
+        }
+    });
