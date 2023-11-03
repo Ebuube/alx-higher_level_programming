aa6d6c42446f207776b2beb797e70d76f093685a
Ebuube <onwutaebubegideon1555@gmail.com>
2023-11-03 01:59:37 +0000

    semistandard

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,20 +1,13 @@
 // Using the core $.ajax() method
 $.ajax({
-    // The URL for the request
-    url: "https://swapi-api.alx-tools.com/api/people/5/?format=json",
+  // The URL for the request
+  url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
 
-    type: "GET",
+  type: 'GET',
 
-    dataType: "json",
+  dataType: 'json'
 })
 
-.done(function(json) {
-    $("DIV#character").text(json.name);
-})
-
-.fail(function(xhr, status, errorThrown) {
-    alert("The request failed!");
-    console.log("Error: " + errorThrown);
-    console.log("Status: " + status);
-    console.dir(xhr);
-})
+  .done(function (json) {
+    $('DIV#character').text(json.name);
+  });
