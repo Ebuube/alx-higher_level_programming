2074c89709fecdd1fc11d7926d2930147624582c
Ebube Gideon Onwuta <onwutaebubegideon1555@gmail.com>
2023-11-03 04:15:33 +0100

    modifed script

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,22 +1,6 @@
-/*
-$("DIV#add_item").bind("click", function() {
-    $("UL.my_list").append("<li>Item</li>");
-})
-
-$("DIV#remove_item").bind("click", function() {
-    $("UL.my_list").remove("li");
-});
-
-$("DIV#clear_list").bind("click", function() {
-    $("UL.my_list").append("<li>Item_new</li>");;
-});
-*/
-
-let count = 0;
 $(function () {
     $("DIV#add_item").click(function() {
         $("UL.my_list").append("<li>Item</li>");
-        count++;
     });
 
     $("DIV#remove_item").bind("click", function() {
