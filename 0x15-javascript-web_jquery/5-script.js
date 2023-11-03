77cad386f50b2aa27f09df3134cdb4dc9af4586e
Ebuube <onwutaebubegideon1555@gmail.com>
2023-11-03 01:38:39 +0000

    Semistandard compliant

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,3 +1,3 @@
-$("DIV#add_item").bind("click", function() {
-    $("UL.my_list").append("<li>Item</li>");
-})
+$('DIV#add_item').bind('click', function () {
+  $('UL.my_list').append('<li>Item</li>');
+});
